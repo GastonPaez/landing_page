fe2210e5db9ac9535a1ed64ccc612c6062762fe3
Gaston <paez.gastonm@gmail.com>
2022-04-02 18:38:11 -0300

    final web

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,18 @@
 (function(){
     const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+
 
     openButton.addEventListener('click', ()=>{
-        alert('')
-    }
-    )
-}
-)();
+        menu.classList.add('nav__link--show');
+
+    });
+
+    closeMenu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+    
+
+})();
